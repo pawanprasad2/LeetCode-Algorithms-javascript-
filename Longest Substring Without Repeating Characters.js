ab4292a35730278abe2d27418147e35d11e8b899
pawan prasad <113040973+pawanprasad2@users.noreply.github.com>
2025-07-02 00:55:13 +0530

    Update Longest Substring Without Repeating Characters.js

--- Longest Substring Without Repeating Characters.js
+++ Longest Substring Without Repeating Characters.js
@@ -10,7 +10,7 @@
 // Output: 1
 // Explanation: The answer is "b", with the length of 1.
 
-
+//without built in func
 var lengthOfLongestSubstring = function(s) {
     let chars = [];
     let left = 0;
@@ -19,7 +19,7 @@ var lengthOfLongestSubstring = function(s) {
     for (let right = 0; right < s.length; right++) {
         let currentChar = s[right];
 
-        // Check if character is already in `chars` between left and right
+  
         for (let i = left; i < right; i++) {
             if (s[i] === currentChar) {
                 left = i + 1;
@@ -35,3 +35,47 @@ var lengthOfLongestSubstring = function(s) {
 
     return maxLength;
 };
+
+
+
+// with built in 
+
+var lengthOfLongestSubstring = function(s) {
+    let current = "";
+    let maxLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let ch = s[i];
+
+        if (current.includes(ch)) {
+            let index = current.indexOf(ch);
+            current = current.slice(index + 1);
+        }
+
+        current += ch;
+
+        if (current.length > maxLength) {
+            maxLength = current.length;
+        }
+    }
+
+    return maxLength;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
